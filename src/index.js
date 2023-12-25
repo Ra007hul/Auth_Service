71b3a080b3946e428923f80c8375b67728cb4b08
unknown <007rahul9254@gmail.com>
2023-12-25 11:02:53 +0530

    Added crate and verify token logic

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const { PORT }= require('./config/serverConfig');
 const bodyParser = require('body-parser');
 const app = express();
 
+const UserService = require('./services/user-service');
+
+
 const prepareAndStartServer = ()=>{
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}))
@@ -12,7 +15,15 @@ const prepareAndStartServer = ()=>{
     
     app.listen(PORT,()=>{
         console.log(`Server started at port ${PORT}`);
+        const service = new UserService();
+        const token = service.createToken({email : "rahul@gmail.com" , id : 1});
+        // console.log(token);
+        const response = service.verifyToken(token);
+        console.log(response);
     })
 }
 
 prepareAndStartServer();
+
+
+
