cc612d2d15a2e4225a181c042c8874a0914ed707
unknown <007rahul9254@gmail.com>
2023-12-26 11:39:58 +0530

    Added roles and user_roles

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const ApiRoutes = require('./routes/index');
 const { PORT }= require('./config/serverConfig');
 const bodyParser = require('body-parser');
 const app = express();
-
+const db = require('./models/index');
 // const UserService = require('./services/user-service');
-
+const {User , Role} =require('./models/index');
 
 const prepareAndStartServer = ()=>{
     app.use(bodyParser.json());
@@ -13,13 +13,17 @@ const prepareAndStartServer = ()=>{
 
     app.use('/api',ApiRoutes);
     
-    app.listen(PORT,()=>{
+    app.listen(PORT,async ()=>{
         console.log(`Server started at port ${PORT}`);
         // const service = new UserService();
         // const token = service.createToken({email : "rahul@gmail.com" , id : 1});
         // // console.log(token);
         // const response = service.verifyToken(token);
         // console.log(response);
+        // db.sequelize.sync({alter:true})
+        const u1 = await User.findByPk(1);
+        const r1 = await Role.findByPk(1);
+        u1.addRole(r1);
     })
 }
 
